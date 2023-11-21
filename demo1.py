06f02b0c5dd50e41191cc3f32b6f200ee47c4197
lukalisanin <luka.lisanin@gmail.com>
2023-11-21 19:48:04 +0000

    updated demo2.py

---
+++ demo1.py
@@ -0,0 +1,9 @@
+print ("Dobro došli u PyDev program obrazovanja")
+
+first_name = "Pero"
+last_name = "Perić"
+date_of_birth = 1998
+mobile_phone = "+385 99 1234 567"
+employed = True
+height = 180.99
+print (first_name, last_name)
